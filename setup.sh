70d0001452706c5f1bd698a0a9c8052b46a9f6a5
Raj Singh <rajsinghcpre@gmail.com>
2025-03-15 14:06:38 -0500

    Add dependency

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ set -e
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# Install Oh My Zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 find . -maxdepth 1 -type d -not -path './.git' -not -path . | cut -d/ -f2- | sort -h | while read -r dir; do
     stow --no-folding -v "${dir}" 2>&1 | { grep -v "BUG in find_stowed_path" || test $? = 1; }
 done
